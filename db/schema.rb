1c3d1d639adb9486bce862106283c6347da30023
emma <emilyaaronlotz@gmail.com>
2023-05-05 17:10:34 -0600

    adding author id to book class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_05_201339) do
+ActiveRecord::Schema.define(version: 2023_05_05_230002) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "birth_year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "name"
